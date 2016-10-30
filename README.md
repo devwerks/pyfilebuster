e9e7ccb2e23609047d1174580c509542dc3f6e6e
blubtxt <admin@hamsterbaum.de>
2016-10-30 14:26:55 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# pyFileBuster
+A fast web fuzzer written in Python
+
+### Why?:
+The Idea behind this is from [Filebuster](https://github.com/henshin/filebuster). But i hate PERL and i want to have the
+same tool written in Python. So here is it. So far not all features are implemented.
+
+### Usage:
+```
+filebuster.py -w/--word WORDLIST -u/--url URL/{fuzz}
+
+Example: filebuster.py -w fast.txt -u http://test.net/
+
+Example: filebuster.py -w wordlist.txt -u http://test.net/{fuzz}.html
+```
+
+### Contact:
+If you run into issues, feel free to get on touch on Twitter, check the current issues or create a new one. Patches are also welcome.
+
+http://devwerks.net 
+
+```
+            ______ _ _     ______           _
+            |  ___(_) |    | ___ \         | |
+ _ __  _   _| |_   _| | ___| |_/ /_   _ ___| |_ ___ _ __
+| '_ \| | | |  _| | | |/ _ \ ___ \ | | / __| __/ _ \ '__|
+| |_) | |_| | |   | | |  __/ |_/ / |_| \__ \ ||  __/ |
+| .__/ \__, \_|   |_|_|\___\____/ \__,_|___/\__\___|_|
+| |     __/ |
+|_|    |___/                                          0.1
+
+Web fuzzer in Python
+
+Author: Johannes Schroeter - www.devwerks.net
+
+[!] legal disclaimer: Usage of pyFilebuster for attacking targets without prior mutual consent is illegal.
+It is the end user's responsibility to obey all applicable local, state and federal laws.
+Developers assume no liability and are not responsible for any misuse or damage caused by this program
+
+filebuster.py -w/--word WORDLIST -u/--url URL/{fuzz}
+Example: filebuster.py -w fast.txt -u http://test.net/
+Example: filebuster.py -w wordlist.txt -u http://test.net/{fuzz}.html
+```
---
+++ filebuster.py
@@ -0,0 +1,122 @@
+import sys, getopt, os, urllib2
+from threading import Thread
+from colors import *
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+useragent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:46.0) Gecko/20100101 Firefox/46.0"
+
+banner = """
+            ______ _ _     ______           _
+            |  ___(_) |    | ___ \         | |
+ _ __  _   _| |_   _| | ___| |_/ /_   _ ___| |_ ___ _ __
+| '_ \| | | |  _| | | |/ _ \ ___ \ | | / __| __/ _ \ '__|
+| |_) | |_| | |   | | |  __/ |_/ / |_| \__ \ ||  __/ |
+| .__/ \__, \_|   |_|_|\___\____/ \__,_|___/\__\___|_|
+| |     __/ |
+|_|    |___/                                          0.1
+
+Web fuzzer in Python
+
+Author: Johannes Schroeter - www.devwerks.net
+
+[!] legal disclaimer: Usage of pyFilebuster for attacking targets without prior mutual consent is illegal.
+It is the end user's responsibility to obey all applicable local, state and federal laws.
+Developers assume no liability and are not responsible for any misuse or damage caused by this program
+
+"""
+
+
+def listWordlists():
+    onlyfiles = [f for f in listdir(BASE_DIR + "/wordlists/") if isfile(join(BASE_DIR + "/wordlists/", f))]
+    print onlyfiles
+
+
+def request(newurl):
+    try:
+        req = urllib2.Request(newurl)
+        req.get_method = lambda: "HEAD"
+        req.add_header("User-agent", useragent)
+        response = urllib2.urlopen(req)
+        data = response.read()
+        sys.stdout.write(GREEN)
+        output = "%s (size: %s)\n" % (newurl.rstrip(), len(data))
+        sys.stdout.write(output)
+        sys.stdout.write(RESET)
+    except urllib2.HTTPError, e:
+        sys.stdout.write(RED)
+        sys.stdout.write("%s\n" % newurl)
+        sys.stdout.write(RESET)
+        # print(e.code)
+    except urllib2.URLError, e:
+        sys.stdout.write(RED)
+        sys.stdout.write("%s\n" % newurl)
+        sys.stdout.write(RESET)
+        # print(e.args)
+
+
+def createurl(url, word):
+    if url.find("{fuzz}") != -1:
+        return url.replace("{fuzz}", word)
+    else:
+        newurl = url + word
+        return newurl
+
+
+def start(url, wordlist):
+    if wordlist != 0:
+        with open(wordlist) as f:
+            for line in f:
+                newurl = createurl(url.rstrip(), line.rstrip())
+                thread = Thread(target=request, args=(newurl,))
+                thread.start()
+                thread.join()
+
+
+def scan():
+    version()
+
+    wordlist = 0
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hu:w:", ["help", "url=", "word="])
+
+    except getopt.GetoptError:
+        help()
+        sys.exit(2)
+
+    for opt, arg in opts:
+
+        if opt in ("-h", "--help"):
+            help()
+            sys.exit()
+
+        elif opt in ("-w", "--word"):
+            path = BASE_DIR + "/wordlists/" + arg
+            if os.path.isfile(path):
+                wordlist = path
+            else:
+                listWordlists()
+                sys.exit()
+
+        elif opt in ("-u", "--url"):
+            start(arg, wordlist)
+
+
+def version():
+    sys.stdout.write(banner)
+
+
+def help():
+    sys.stdout.write("filebuster.py -w/--word WORDLIST -u/--url URL/{fuzz}\n")
+    sys.stdout.write("Example: filebuster.py -w fast.txt -u http://test.net/\n")
+    sys.stdout.write("Example: filebuster.py -w wordlist.txt -u http://test.net/{fuzz}.html\n\n")
+
+
+def main():
+    scan()
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
